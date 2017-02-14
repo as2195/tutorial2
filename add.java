692a97b0dd4c73cde31461ef3d8c8f4e6cb268f0
as2195 <albertano.santos@gmail.com>
2017-02-13 20:17:51 -0500

    Create add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add{
+  public static void main(String[] args){
+    System.out.print("Enter number: ");
+    double num1=10;
+    System.out.print("Enter number: ");
+    double num2=5;
+    double add=num1+num2;
+    System.out.println(add);
+  }
+}
